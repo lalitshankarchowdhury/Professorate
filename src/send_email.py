31108f70012bdbb5110d23c23d3d24a9714ccab1
Lalit Shankar Chowdhury <lalitshankarch@gmail.com>
2022-08-22 22:53:24 +0530

    Add timer

--- src/send_email.py
+++ src/send_email.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import os.path
 import base64
+import time
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -13,22 +14,26 @@ from googleapiclient.errors import HttpError
 SCOPES = ["https://www.googleapis.com/auth/gmail.compose"]
 CLIENT_CREDS = "../auth/email/client_creds.json"
 ACCESS_TOKEN = "../auth/email/access_token.json"
+creds = None
 
-def send_email():
-    """Send email"""
-    creds = None
+
+def auth_client():
+    """Authorize client"""
+    global creds
     if os.path.exists(ACCESS_TOKEN):
         creds = Credentials.from_authorized_user_file(ACCESS_TOKEN, SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                CLIENT_CREDS, SCOPES
-            )
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_CREDS, SCOPES)
             creds = flow.run_local_server(port=0)
         with open(ACCESS_TOKEN, "w") as token:
             token.write(creds.to_json())
+
+
+def send_email():
+    """Send email"""
     try:
         service = build("gmail", "v1", credentials=creds)
         message = EmailMessage()
@@ -49,4 +54,11 @@ def send_email():
 
 
 if __name__ == "__main__":
-    send_email()
+    auth_client()
+    prev_time_in_mins = 0
+    while True:
+        time_in_mins = time.time() // 60
+        if time_in_mins % 5 == 0 and time_in_mins != prev_time_in_mins:
+            send_email()
+            prev_time_in_mins = time_in_mins
+        time.sleep(1)
